c9211e19d46b347033d9bd0f2af8eed1d1864e3f
AyalaSprei <ayala0bsd@gmail.com>
2024-01-24 13:25:54 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int findSingle(int [] arr){
+        int sum =0;
+        int length = arr.length;
+        for(int i=0; i<length; i++){
+            sum+=arr[i];
+        }
+        int maxSingle=sum-length/2 + sum%2;
+
+        for(int i = maxSingle;i>0;i--){
+            for(int j=0; j<arr.length; j+=2){
+                if(arr[j]==i){
+                    return arr[i];
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello world!");
+
+        System.out.println("Enter the size of the array:");
+        int size = scanner.nextInt();
+
+        System.out.println("Enter the elements of the array:");
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Result: " + findSingle(arr));
+    }}
