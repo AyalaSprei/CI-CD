1ffcebada90a6abfb19ce8a79d9850962d28c9da
AyalaSprei <ayala0bsd@gmail.com>
2024-01-24 13:55:29 +0200

    nothing

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        System.out.println("Hello worldd!");
 
         System.out.println("Enter the size of the array:");
         int size = scanner.nextInt();
